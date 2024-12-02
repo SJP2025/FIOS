4dd208b4505cce054b898712dd6df566682dd366
SJP2025 <stevepladl@aol.com>
2024-12-02 08:38:16 -0500

    https://github.com/SJP2025/FIOS.git

--- Testing.ps1
+++ Testing.ps1
@@ -1,21 +1,6 @@
 
 
-<#
-spladl@Brandi-PC:/mnt/c/Users/compu/Desktop/FIOS$ git config --list
-user.name=SJP2025
-user.email=stevepladl@aol.com
-core.repositoryformatversion=0
-core.filemode=false
-core.bare=false
-core.logallrefupdates=true
-core.symlinks=false
-core.ignorecase=true
-remote.origin.url=https://github.com/SJP2025/FIOS.git
-remote.origin.fetch=+refs/heads/*:refs/remotes/origin/*
-user.name=SJP2025
-user.email=stevepladl@aol.com
-spladl@Brandi-PC:/mnt/c/Users/compu/Desktop/FIOS$
-#>
+
 
 $hello = write-host "hello world"
-
+$hello
