79811c6480e1f81e79ac129384c961be116d4b53
SJP2025 <stevepladl@aol.com>
2024-12-03 22:25:52 -0500

    Editing Code Region

--- Invoke-Lib/Initiate-Process.ps1
+++ Invoke-Lib/Initiate-Process.ps1
@@ -1,8 +1,6 @@
 #Clean and generate/regenerate absolute Path files (.rtr files)
-#Region Generate router files
 $DrivePathLib = $PSScriptRoot
 Write-Output "Sourcing Drive Path: ${DrivePathLib}"
-#EndRegion
 #Generate Drive path ".rtr" files (Absolute path reference files)
 #Region Generate Absolute PAth Reference
 Function Initiate-Process {
